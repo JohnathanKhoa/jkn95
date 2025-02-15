8e8328322971f330517d6879c556633e29a49008
JohnathanKhoa <johnathan.k.nguyen.95@gmail.com>
2025-02-14 18:57:58 -0800

    fix page error

--- src/app/demos/(nurtree)/nurtree/(authenticated)/playlists/[...playlistId]/page.tsx
+++ src/app/demos/(nurtree)/nurtree/(authenticated)/playlists/[...playlistId]/page.tsx
@@ -27,15 +27,6 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-export function setCurrentVideo({id}: YoutubeVideo) {
-    return (
-      <div className="width: 100%; min-width: 400px; max-width: 800px;">
-        <div className="position: relative; width: 100%; overflow: hidden; padding-top: 56.25%;">
-          <p><iframe className="position: absolute; top: 0; left: 0; right: 0; width: 100%; height: 100%; border: none;" src={`https://www.youtube.com/embed/${id}`} width="560" height="315" allowfullscreen="allowfullscreen" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe></p>
-        </div>
-      </div>
-    )
-}
 
 
 export default async function PlaylistPage({ params }: Props) {
