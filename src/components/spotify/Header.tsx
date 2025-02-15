e650765b3d6a1101c98b152483e5f32c4bfa6934
JohnathanKhoa <johnathan.k.nguyen.95@gmail.com>
2025-02-14 20:15:09 -0800

    change url

--- src/components/spotify/Header.tsx
+++ src/components/spotify/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   const router = useRouter();
 
   const logout = () => {
-    signOut({ callbackUrl: "http://localhost:3000/demos/nurtree/login" });
+    signOut({ callbackUrl: "https://jkn95-blond.vercel.app/demos/nurtree/login" });
   };
 
   return (
