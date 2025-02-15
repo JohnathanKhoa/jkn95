82ee5eb18a380028037a21bdad3f6ef6a6839147
JohnathanKhoa <johnathan.k.nguyen.95@gmail.com>
2025-02-14 23:27:47 -0800

    contact page

---
+++ src/app/contact/contact.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Contact from '../markdown/contact.mdx'
+
+export default function Page() {
+    return (
+        <>
+        <div className="w-1/2 justify-self-center font-serif antialiased opacity-80">
+            <Contact className="font-serif"/>
+        </div>
+        
+        </>
+    )
+}
