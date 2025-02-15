492b583d46cdc9d41a3f3e4501ce376d5928bd3f
JohnathanKhoa <johnathan.k.nguyen.95@gmail.com>
2025-02-14 18:29:40 -0800

    fix auth

--- src/app/demos/(nurtree)/nurtree/[nurtree]/page.tsx
+++ src/app/demos/(nurtree)/nurtree/[nurtree]/page.tsx
@@ -4,6 +4,9 @@ import React from "react";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import SpotifyImage from '@/public/images/spotify_logo.png'
+import YoutubeImage from '@/public/images/yt_logo_mono_dark.png'
+import NurtreeImage from '@/public/images/nurtree.png'
+import Link from "next/link";
 
 export default function Page() {
   const handleLogin = () => {
@@ -12,19 +15,46 @@ export default function Page() {
 
   return (
     <>
-    <div className="pb-64 flex flex-col items-center justify-center w-screen h-screen gap-20">
-      <Image
-        src={SpotifyImage}
-        alt="spotify logo"
-        width={320}
-        height={96}
-      />
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-20">
+      <div className="text-center text-balance flex flex-col items-center justify-center">
+      Play all the top music videos using your own curated Spotify playlists
+      </div>
+      <div className="flex md:flex-row flex-col w-screen items-center justify-center gap-x-6">
+        Nurtree, a blend of
+        <Image
+          src={SpotifyImage}
+          alt="spotify logo"
+          width={320}
+          height={96}
+        />
+      <a className="flex flex-col items-center justify-center text-3xl">
+        +
+      </a>
+        <Image
+          src={YoutubeImage}
+          alt="youtube logo"
+          width={320}
+          height={96}
+        />
+        
+      </div>
       <button
-        className="flex px-12 py-2 text-lg tracking-widest uppercase rounded-full focus:outline-none bg-primary hover:bg-opacity-80"
+        className="hover:underline flex px-12 pt-20 text-3xl tracking-widest uppercase rounded-full focus:outline-none bg-primary hover:bg-opacity-80"
         onClick={handleLogin}
       >
         Login
       </button>
+      <div className="">
+        with Premium Spotify.
+      </div>
+      <div>
+      <div className="text-center text-balance flex-col items-center justify-center">
+        Don't have an account? <Link className="underline" href="https://www.spotify.com/us/signup?forward_url=https%3A%2F%2Fopen.spotify.com%2F">Sign up</Link> on their official website
+      </div>
+      {/* <div className="text-center text-balance">
+        <Link className="underline" href="">Use demo features</Link> without an account
+      </div> */}
+      </div>
     </div>
     </>
   );
