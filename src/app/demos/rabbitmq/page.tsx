bafc7126c89edf3c6c2f241819c203de8a124e7b
JohnathanKhoa <johnathan.k.nguyen.95@gmail.com>
2025-04-03 05:30:06 -0700

    GitHub Link RabbitMQ

--- src/app/demos/rabbitmq/page.tsx
+++ src/app/demos/rabbitmq/page.tsx
@@ -8,6 +8,11 @@ export default function Page() {
         Provide your email to recieve a message from the RabbitMQ broker
       </p> */}
       <RabbitMQForm />
+      <a className="p-3" href="https://github.com/JohnathanKhoa/amqp" target="_blank">
+        <button className=" rounded-xl text-black hover:text-zinc-300 hover:bg-black transition duration-300 bg-white p-3">
+          GitHub
+        </button>
+      </a>
     </div>
   );
 }
